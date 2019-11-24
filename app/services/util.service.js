a4f8ad227ca50f4b948dacb81341090c6c4203cf
chandan mishra <cmishra646@gmail.com>
2019-11-25 02:16:13 +0530

    added throw error and async to in util service

---
+++ app/services/util.service.js
@@ -0,0 +1,20 @@
+const { to } = require('await-to-js');
+const pe = require('parse-error');
+
+module.exports.to = async promise => {
+  let err, res;
+  [err, res] = await to(promise);
+  if (err) return [pe(err)];
+
+  return [null, res];
+};
+
+module.exports.TE = TE = function(err_message, log) {
+  // TE stands for Throw Error
+  if (log === true) {
+    console.error(err_message);
+  }
+
+  throw new Error(err_message);
+};
+
